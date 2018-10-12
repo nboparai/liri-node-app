335c62acc837cf38f5c14945fc8a75d5559eb3b4
Navreet Boparai <navreet.boparai@yahoo.com>
2018-10-11 18:10:00 -0700

    2nd commit

--- keys.js
+++ keys.js
@@ -1,9 +1,13 @@
-console.log("in keys file");
+// console.log("in keys file");
 exports.spotify = {
     id: process.env.SPOTIFY_ID, 
-    secret: process.env.SPOTIFY_SECRET
-}
+    secret: process.env.SPOTIFY_SECRET,
+};
 
 exports.odmb = {
     id: process.env.ODMB_ID
+};
+
+exports.bandsInTown = {
+    id: process.env.BANDS_ID
 }
--- liri.js
+++ liri.js
@@ -5,30 +5,38 @@ var request = require("request");
 var moment = require("moment");
 var fs = require("fs");
 var spotify = new Spotify(keys.spotify);  //initializing spotify -passing keys to variable
-
 var action = process.argv[2];
 var title = process.argv.slice(3).join(" ");
+
 function getBandsInTown(){
+    var bandKey = keys.bandsInTown.id;
    var queryUrl=
-    "https://rest.bandsintown.com/artists/" + title + "/events?app_id=codingbootcamp";
-    console.log(queryUrl);
+    "https://rest.bandsintown.com/artists/" + title + "/events?app_id=" +bandKey;
     request(queryUrl, function(error, response, body) {
 
         // If the request is successful
         if (!error && response.statusCode === 200) {
-            var output = JSON.parse(body)[0];
-            console.log(output);
-            console.log("Name of the Venue: " + output.venue.name);
-            console.log("Venue location: " + output.venue.city);
-            console.log("Time of Event: " + moment(output.datetime).format("MM/DD/YYYY"));
+            var output = JSON.parse(body);
+            
+            if (output.length == 0){
+                console.log(`No matching concerts for ${title}`);
+                return;
+            }
+            console.log(`\nUpcoming concerts for ${title}:`);
+            output.forEach((item) =>{
+                console.log("\nName of the Venue: " + item.venue.name);
+                console.log("Venue location: " + item.venue.city + ", " + (item.venue.region || item.venue.country));
+                console.log("Time of Event: " + moment(item.datetime).format("MM/DD/YYYY"));
+            });
+            
         }
 
       });
     
 }
 function getSpotify(){
-    if (title ===undefined) {
-        title = "Ace of Base"
+    if (!title) {
+        title = "Ace of Base";
     }
     
     spotify.search({ type: 'track', query: title, limit: 10  }, function(err, data) {
@@ -36,39 +44,49 @@ function getSpotify(){
         return console.log('Error occurred: ' + err);
         }
 
-        var songArray = [];
 
         for (var i = 0; i < data.tracks.items.length; i++ ) {
-            var result = {
-                artist : data.tracks.items[i].album.artists[0].name,
-                album_name : data.tracks.items[i].album.name,
-                song_name : data.tracks.items[i].name,
-                preview_url : data.tracks.items[i].preview_url 
-            }
-            songArray.push(result);
+            var artistArr = data.tracks.items[i].artists;
+            var artistName = [];
+               artistArr.forEach((item) => {
+                   artistName.push(item.name);
+                   console.log(`artists : ${item.name}`);
+
+               });
+               console.log(`\nartists : ${artistName.join(", ")}`);
+                console.log(`album_name : ${data.tracks.items[i].album.name}`);
+                console.log(`song name : ${data.tracks.items[i].name}`);
+                console.log(`preview_url : ${data.tracks.items[i].preview_url}`);
             
         }
-        console.log(songArray);
+        
     });
 }
 function getOdmb(){
-    var queryUrl = `http://www.omdbapi.com/?t=${title}&apikey=7587dcf2`;
-    console.log(queryUrl);
-    request(queryUrl, function(error, response, body) {
+    if (!title) {
+        title = "Mr. Nobody";
+        console.log(`\nIf you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>`);
+        console.log(`\nIt's on Netflix`);
+    }
+    var odmbKey = keys.odmb.id;
+    var queryUrl = `http://www.omdbapi.com/?t=${title}&apikey=${odmbKey}`;
+        request(queryUrl, function(error, response, body) {
 
         // If the request is successful
         if (!error && response.statusCode === 200) {
       
           // Parse the body of the site and recover teh data needed
-          
-          console.log("Ttile: " + JSON.parse(body).Title);
-          console.log("Release Year: " + JSON.parse(body).Year);
-          console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-          console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-          console.log("Produced in: " + JSON.parse(body).Country);
-          console.log("Language: " + JSON.parse(body).Language);
-          console.log("Plot: " + JSON.parse(body).Plot);
-          console.log("Actors: " + JSON.parse(body).Actors);
+          var output =  JSON.parse(body);
+          console.log("\nTtile: " + output.Title);
+          console.log("Release Year: " + output.Year);
+          console.log("IMDB Rating: " + output.imdbRating);
+          if (output.Ratings[1]){
+             console.log("Rotten Tomatoes Rating: " + output.Ratings[1].Value);
+          }
+          console.log("Produced in: " + output.Country);
+          console.log("Language: " + output.Language);
+          console.log("Plot: " + output.Plot);
+          console.log("Actors: " + output.Actors);
         }
 
       });
